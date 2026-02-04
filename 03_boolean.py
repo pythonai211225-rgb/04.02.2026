f3d7eed19eb89fa4c23b02bc90f6e926e4864db5
fullstack290623 <fullstack290623@gmail.com>
2026-02-04 18:44:02 +0200

    upload

--- 03_boolean.py
+++ 03_boolean.py
@@ -9,4 +9,59 @@ else:
 
 print(age > 18)
 
-is_adult: bool = age > 18  # eager
+# ===================== lazy behavior
+age = 20
+is_adult: bool = age > 18  # lazy  Not computed yet
+age = 16
+print(is_adult)  # lazy -- False computed now
+age = 20
+print(is_adult)  # lazy -- True computed now
+
+# ===================== eager behavior
+age = 20
+is_adult: bool = age > 18  # eager  True - computed instantly , never again
+age = 16
+print(is_adult)  # eager -- True
+
+# lazy
+# for _ in range(age):
+#   pass
+
+if is_adult == True:
+    print('Beer')
+else:
+    print('Coke')
+
+if is_adult:
+    print('Beer')
+else:
+    print('Coke')
+
+if is_adult == False:
+    print('Coke')
+else:
+    print('Beer')
+
+if not is_adult:
+    print('Coke')
+else:
+    print('Beer')
+
+# input rating from the user, the range should be 1-5
+#   is_valid should be True if in range and False if not in range
+#   if is_valid:
+#      print('in range')
+#   else:
+#      print('not in range')
+#
+# is_best should be True if the rating was 5
+#   if is_best:
+#      print('highest score')
+#   else:
+#      print('not highest score')
+#
+# is_medium should be True if the rating was between 2-4
+#   if not is_medium:
+#      print('score high or low')
+#   else:
+#      print('medium score')
